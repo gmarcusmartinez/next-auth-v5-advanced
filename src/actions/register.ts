efb432e3affca63dbc0dbb0d6f588939c92ce9dc
Marcus Martinez <gmarcusmartinez@gmail.com>
2024-05-17 08:49:40 +0200

    feat:create user

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from "@/schemas"
 import * as z from "zod"
+import bcrypt from "bcryptjs"
+import { db } from "@/lib/db"
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validated = RegisterSchema.safeParse(values)
@@ -9,5 +12,17 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid form data" }
   }
 
-  return { success: "Email sent!" }
+  const { email, password, name } = validated.data
+  const hashed = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+  if (existingUser) return { error: "Email already in use." }
+
+  await db.user.create({
+    data: { email, password: hashed, name },
+  })
+
+  //TODO: Send email confirmation
+
+  return { success: "User created!" }
 }
